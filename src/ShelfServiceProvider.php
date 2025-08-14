1cab3ded8fb40cafa9e7fcbefb606ff69ab8eb60
Hosein <hetbo98@yahoo.com>
2025-08-14 21:08:42 +0000

    Added Service Provider

---
+++ src/ShelfServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Hetbo\Shelf;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShelfServiceProvider extends ServiceProvider {
+    public function register() {
+
+    }
+
+    public function boot() {
+
+    }
+}
